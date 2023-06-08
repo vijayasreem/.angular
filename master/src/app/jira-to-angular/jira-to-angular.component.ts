a40577770a118c710e5309112e66e83337026feb
dotnet-code <@dotnet-code>
2023-06-08 14:28:16 +0530

    commit from code202306082816

---
+++ master/src/app/jira-to-angular/jira-to-angular.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jira-to-angular',
+  templateUrl: './jira-to-angular.component.html',
+  styleUrls: ['./jira-to-angular.component.css']
+})
+export class JiraToAngularComponent implements OnInit {
+
+  errorMessage: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.errorMessage = '\'NoneType\' object has no attribute \'replace\'';
+  }
+
+}
